4c49b69d8b4e5c4c3ccbfca7a393535d80f6a102
ahmad movahedi <kevetsahedrakh@hotmail.com>
2023-06-20 20:00:15 +0330

    add data for foods

--- form/src/components/cardProduct/index.jsx
+++ form/src/components/cardProduct/index.jsx
@@ -1,16 +1,28 @@
 import Counter from "../counter";
 
-const CardProduct = () => {
+const CardProduct = ({ name, price, img, description }) => {
   const rtl = {
     direction: "rtl",
   };
   return (
-    <div className="w-72 rounded overflow-hidden bg-white " style={rtl}>
-      <div className="px-4 py-4 flex flex-col items-start">
-        <div className="">title</div>
-        <div className="text-slate-400 text-lg italic">subtitle</div>
-        <p className="text-base">description</p>
-        <Counter />
+    <div
+      className="w-60 rounded overflow-hidden flex flex-col bg-white "
+      style={rtl}
+    >
+      <div>
+        <img src={img} />
+      </div>
+      <div className="flex flex-col items-start">
+        <div className="px-4 pt-4 font-medium">{name}</div>
+        <div className="px-4 text-lg italic">
+          <span className=" text-balck">قیمت</span>
+          <span className="text-red-600">{price}</span>
+          <span className=" text-balck">ریال</span>
+        </div>
+        <div className="px-4 text-base text-right">{description}</div>
+        <div className="px-4 pb-2 pt-4">
+          <Counter />
+        </div>
       </div>
     </div>
   );
--- form/src/components/products/index.jsx
+++ form/src/components/products/index.jsx
@@ -1,16 +1,60 @@
 import CardProduct from "../cardProduct";
 
 const Products = () => {
-  const bergers = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
+  const bergers = [
+    {
+      img: "https://www.qfcfastfood.com/assets/images/goods/000000/good_local_1190-500x357-6762.jpg",
+      name: "اسنک اسپایسی",
+      price: "1,800,000",
+      description: "2تکه مرغ سوخاري سيب زميني سالاد کلم",
+    },
+    {
+      img: "https://www.qfcfastfood.com/assets/images/goods/000000/good_local_1191-500x357-6763.jpg",
+      name: "دینر اسپایسی",
+      price: "1,990,000",
+      description: "3 تکه مرغ سوخاري سيب زميني سالاد سيب زميني سالاد کلم",
+    },
+    {
+      img: "https://www.qfcfastfood.com/assets/images/goods/000000/good_local_1192-500x357-6875.jpg",
+      name: "فامیلی اسپایسی",
+      price: "3,990,000",
+      description: "7تکه مرغ سوخاري2عدد سيب زميني2عدد",
+    },
+    {
+      img: "https://www.qfcfastfood.com/assets/images/goods/000000/good_local_1193-500x357-6859.jpg",
+      name: "استریپس نرمال",
+      price: "1,990,000",
+      description: "4تکه فيله سوخاري سيب زميني سالاد",
+    },
+    {
+      img: "https://www.qfcfastfood.com/assets/images/goods/000000/good_local_1287-500x357-6919.jpg",
+      name: "کالزونه",
+      price: "1,790,000",
+      description: "زامبون گوشت زامبون مرغ پپروني قارچ ",
+    },
+    {
+      img: "https://www.qfcfastfood.com/assets/images/goods/000000/good_local_1203-500x357-6773.jpg",
+      name: "پیتزا پپرونی",
+      price: "1,840,000",
+      description: "پپروني مخصوص قارچ مخصوص پنير فلفل دلمه ذرت",
+    },
+  ];
   return (
     <div className="bg-alice-blue-50 w-full h-full rounded-xl flex flex-col p-2 ">
       <header className="text-2xl font-semibold p-2">
         رستوران مک دونالد شعبه بندر
       </header>
       <hr className="font-semibold bg-black" />
-      <div className="grid grid-cols-3 gap-1 mt-1">
+      <div className="grid grid-cols-3 gap-2 mt-1">
         {bergers.map((elment) => {
-          return <CardProduct />;
+          return (
+            <CardProduct
+              name={elment.name}
+              img={elment.img}
+              price={elment.price}
+              description={elment.description}
+            />
+          );
         })}
       </div>
     </div>
