d8dd318f7e869220838ee9cf7d4ae06a4e3b507a
ahmad movahedi <kevetsahedrakh@hotmail.com>
2023-06-24 16:19:48 +0330

    add counter

--- form/src/components/counter/index.jsx
+++ form/src/components/counter/index.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+
 const Counter = () => {
+  const [count, setcount] = useState(0);
+  function inCrise() {
+    setcount(count + 1);
+  }
+  function diCrise() {
+    if (count > 0) {
+      setcount(count - 1);
+    }
+  }
   return (
     <div className="flex items-center justify-center">
-      <button className="bg-red-600 text-white w-4 h-5 flex items-center justify-center">
+      <button
+        className="bg-red-600 text-white w-4 h-5 flex items-center justify-center"
+        onClick={diCrise}
+      >
         -
       </button>
-      <span className="bg-alice-blue-50 w-10">0</span>
-      <button className="bg-red-600 text-white w-4 h-5 items-center justify-center">
+      <span className="bg-alice-blue-50 w-10">{count}</span>
+      <button
+        className="bg-red-600 text-white w-4 h-5 items-center justify-center"
+        onClick={inCrise}
+      >
         +
       </button>
     </div>
